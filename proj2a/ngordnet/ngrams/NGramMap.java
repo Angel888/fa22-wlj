51f48f83725aade13b041f30ca6eba6fdb1c89ff
weiliangjie <weiliangjie@yuanbaobaoxian.com>
2024-02-07 11:38:39 +0800

    帮我改改 不知道哪里有问题 我觉得你比chatgpt写得好

--- proj2a/ngordnet/ngrams/NGramMap.java
+++ proj2a/ngordnet/ngrams/NGramMap.java
@@ -1,104 +1,120 @@
 package ngordnet.ngrams;
 
-import ngordnet.hugbrowsermagic.LineIterator;
+import edu.princeton.cs.algs4.In;
 
-import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Scanner;
-
-/** An object that provides utility methods for making queries on the
- *  Google NGrams dataset (or a subset thereof).
+import java.util.*;
+//todo 帮我改改 不知道哪里有问题 我觉得你比chatgpt写得好
+/**
+ * An object that provides utility methods for making queries on the
+ * Google NGrams dataset (or a subset thereof).
+ * <p>
+ * An NGramMap stores pertinent data from a "words file" and a "counts
+ * file". It is not a map in the strict sense, but it does provide additional
+ * functionality.
  *
- *  An NGramMap stores pertinent data from a "words file" and a "counts
- *  file". It is not a map in the strict sense, but it does provide additional
- *  functionality.
- *
- *  @author Josh Hug
+ * @author Josh Hug
  */
 // 使用自定义类型 而非嵌套的 HashMap or TreeMap来表达复杂的数据结构
-public class NGramMap{
-    /** Constructs an NGramMap from WORDSFILENAME and COUNTSFILENAME. */
-    public  String word;
-    public ArrayList<Factor> factors;
-    public class Factor{
-        int year;
-        int times;
-        int sources;
 
-    }
-    public NGramMap(String wordsFilename, String countsFilename) {
-        // very_short.csv第3行是times 第4行是sources
-        //total_counts.csv 年份，次数，出现的页数，来源的个数
-        try {
-            BufferedReader fileReader =new BufferedReader(new FileReader(wordsFilename));
-            String line;
-            while ((line = fileReader.readLine()) != null) {
-                line.split();
+public class NGramMap<String, ArrayList<Factor>>{
+/**
+ * Constructs an NGramMap from WORDSFILENAME and COUNTSFILENAME.
+ */
 
-                }
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+public static String word;
+public static ArrayList<Factor> factors;
 
+public static class Factor {
+    int year;
+    int times;
+    int sources;
 
+    public Factor(int year, int times, int numSources) {
+        this.year = year;
+        this.times = times;
+        this.sources = numSources;
     }
 
 
+}
 
-    /** Provides the history of WORD. The returned TimeSeries should be a copy,
-     *  not a link to this NGramMap's TimeSeries. In other words, changes made
-     *  to the object returned by this function should not also affect the
-     *  NGramMap. This is also known as a "defensive copy". */
-    public TimeSeries countHistory(String word) {
 
-        //todo
+public NGramMap(String wordsFilename, String countsFilename) {
+    // very_short.csv第3行是times 第4行是sources
+    //total_counts.csv 年份，次数，出现的页数，来源的个数
+    In in1 = new In(wordsFilename);
 
-        return null;
+    String lineStr = in1.readLine();
+    while (lineStr != null) {
+        String[] aa = lineStr.split("\t"); // 这里的String[] 是一个array
+
     }
 
-    /** Provides the history of WORD between STARTYEAR and ENDYEAR, inclusive of both ends. The
-     *  returned TimeSeries should be a copy, not a link to this NGramMap's TimeSeries. In other words,
-     *  changes made to the object returned by this function should not also affect the
-     *  NGramMap. This is also known as a "defensive copy". */
-    public TimeSeries countHistory(String word, int startYear, int endYear) {
-        return null;
-    }
 
-    /** Returns a defensive copy of the total number of words recorded per year in all volumes. */
-    public TimeSeries totalCountHistory() {
-        return null;
-    }
+}
 
-    /** Provides a TimeSeries containing the relative frequency per year of WORD compared to
-     *  all words recorded in that year. */
-    public TimeSeries weightHistory(String word) {
-        return null;
-    }
+/**
+ * Provides the history of WORD. The returned TimeSeries should be a copy,
+ * not a link to this NGramMap's TimeSeries. In other words, changes made
+ * to the object returned by this function should not also affect the
+ * NGramMap. This is also known as a "defensive copy".
+ */
+public TimeSeries countHistory(String word) {
 
-    /** Provides a TimeSeries containing the relative frequency per year of WORD between STARTYEAR
-     *  and ENDYEAR, inclusive of both ends. */
-    public TimeSeries weightHistory(String word, int startYear, int endYear) {
-        return null;
-    }
+    //todo
 
-    /** Returns the summed relative frequency per year of all words in WORDS. */
-    public TimeSeries summedWeightHistory(Collection<String> words) {
-        return null;
-    }
+    return null;
+}
 
-    /** Provides the summed relative frequency per year of all words in WORDS
-     *  between STARTYEAR and ENDYEAR, inclusive of both ends. If a word does not exist in
-     *  this time frame, ignore it rather than throwing an exception. */
-    public TimeSeries summedWeightHistory(Collection<String> words,
-                              int startYear, int endYear) {
-        return null;
-    }
+/**
+ * Provides the history of WORD between STARTYEAR and ENDYEAR, inclusive of both ends. The
+ * returned TimeSeries should be a copy, not a link to this NGramMap's TimeSeries. In other words,
+ * changes made to the object returned by this function should not also affect the
+ * NGramMap. This is also known as a "defensive copy".
+ */
+public TimeSeries countHistory(String word, int startYear, int endYear) {
+    return null;
+}
+
+/**
+ * Returns a defensive copy of the total number of words recorded per year in all volumes.
+ */
+public TimeSeries totalCountHistory() {
+    return null;
+}
+
+/**
+ * Provides a TimeSeries containing the relative frequency per year of WORD compared to
+ * all words recorded in that year.
+ */
+public TimeSeries weightHistory(String word) {
+    return null;
+}
+
+/**
+ * Provides a TimeSeries containing the relative frequency per year of WORD between STARTYEAR
+ * and ENDYEAR, inclusive of both ends.
+ */
+public TimeSeries weightHistory(String word, int startYear, int endYear) {
+    return null;
+}
+
+/**
+ * Returns the summed relative frequency per year of all words in WORDS.
+ */
+public TimeSeries summedWeightHistory(Collection<String> words) {
+    return null;
+}
+
+/**
+ * Provides the summed relative frequency per year of all words in WORDS
+ * between STARTYEAR and ENDYEAR, inclusive of both ends. If a word does not exist in
+ * this time frame, ignore it rather than throwing an exception.
+ */
+public TimeSeries summedWeightHistory(Collection<String> words,
+                                      int startYear, int endYear) {
+    return null;
+}
 
 
 }
